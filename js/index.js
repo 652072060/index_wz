b96f9cae983ebfdfe96c2f12d3d9059759de9874
loulan0 <652072060@qq.com>
2021-06-03 18:28:23 +0800

    2021.6.3

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+        var _hmt = _hmt || [];
+        (function() {
+          var hm = document.createElement("script");
+          hm.src = "https://hm.baidu.com/hm.js?5fe2e1be0d12bcb71d98ecf0d1beb8c6";
+          var s = document.getElementsByTagName("script")[0]; 
+          s.parentNode.insertBefore(hm, s);
+        })();
